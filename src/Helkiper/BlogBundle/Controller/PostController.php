1faf65150443a5374e1a2675ce167e4fa08fbd31
Hellkiper <helkiper@gmail.com>
2018-02-10 08:19:33 +0200

    move views back to app/Resources

--- src/Helkiper/BlogBundle/Controller/PostController.php
+++ src/Helkiper/BlogBundle/Controller/PostController.php
@@ -22,7 +22,7 @@ class PostController extends Controller
 
         $posts = $em->getRepository('HelkiperBlogBundle:Post')->findAll();
 
-        return $this->render('@HelkiperBlog/post/index.html.twig', array(
+        return $this->render('post/index.html.twig', array(
             'posts' => $posts,
         ));
     }
@@ -78,7 +78,7 @@ class PostController extends Controller
         if ($editForm->isSubmitted() && $editForm->isValid()) {
             $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('post_edit', array('id' => $post->getId()));
+            return $this->redirectToRoute('post_show', array('id' => $post->getId()));
         }
 
         return $this->render('post/edit.html.twig', array(
@@ -103,7 +103,7 @@ class PostController extends Controller
             $em->flush();
         }
 
-        return $this->redirectToRoute('post_index');
+        return $this->redirectToRoute('helkiper_blog_index');
     }
 
     /**
--- src/Helkiper/BlogBundle/Entity/Category.php
+++ src/Helkiper/BlogBundle/Entity/Category.php
@@ -46,6 +46,11 @@ class Category
     	$this->posts = new ArrayCollection();
     }
 
+    public function __toString()
+    {
+	    return $this->name;
+    }
+
 	/**
      * Get id.
      *
