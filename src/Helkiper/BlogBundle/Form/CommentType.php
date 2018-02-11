707efa7f24430720206bf9884b56202598fab9d5
Hellkiper <helkiper@gmail.com>
2018-02-11 11:33:11 +0200

    add transformer for Category

--- src/Helkiper/BlogBundle/Form/CommentType.php
+++ src/Helkiper/BlogBundle/Form/CommentType.php
@@ -2,18 +2,35 @@
 
 namespace Helkiper\BlogBundle\Form;
 
+use Doctrine\DBAL\Types\TextType;
+use Helkiper\BlogBundle\Form\DataTransformer\CategoryToNumberTransformer;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CommentType extends AbstractType
 {
-    /**
+	private $categoryTransformer;
+
+	public function __construct(CategoryToNumberTransformer $categoryTransformer)
+	{
+		$this->categoryTransformer = $categoryTransformer;
+	}
+
+	/**
      * {@inheritdoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('author')->add('content')->add('createdAt')->add('post')->add('category');
+        $builder
+	        ->add('author')
+	        ->add('content')
+	        ->add('category', HiddenType::class)
+	        ->add('post', HiddenType::class);
+
+        $builder->get('category')->addModelTransformer($this->categoryTransformer);
     }/**
      * {@inheritdoc}
      */
---
+++ src/Helkiper/BlogBundle/Form/DataTransformer/PostToNumberTransformer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hellkiper
+ * Date: 11.02.2018
+ * Time: 11:25
+ */
+
+namespace Helkiper\BlogBundle\Form\DataTransformer;
+
+
+class PostToNumberTransformer
+{
+
+}
