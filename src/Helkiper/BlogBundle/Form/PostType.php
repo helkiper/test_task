21472bc5411745dfb1d47bff99e5f0d839471971
Hellkiper <helkiper@gmail.com>
2018-02-13 10:21:46 +0200

    add file uploads

--- src/Helkiper/BlogBundle/Form/PostType.php
+++ src/Helkiper/BlogBundle/Form/PostType.php
@@ -3,6 +3,7 @@
 namespace Helkiper\BlogBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,11 @@ class PostType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name')->add('file')->add('category')->add('content');
+        $builder
+	        ->add('name')
+	        ->add('file', FileType::class)
+	        ->add('category')
+	        ->add('content');
     }/**
      * {@inheritdoc}
      */
